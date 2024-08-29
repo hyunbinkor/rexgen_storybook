7a2898d1dbf3b406dc2ddb00afb7e34125973b46
Hyunbin Jun <gosqlsz@gmail.com>
2024-08-29 16:27:15 +0900

    💬 annotation added

--- .storybook/ThemeContext.tsx
+++ .storybook/ThemeContext.tsx
@@ -1,9 +1,14 @@
-import React, { createContext, useContext, ReactNode } from 'react';
-import theme from './theme';
+import React, { createContext, useContext, ReactNode } from "react";
+import theme from "./theme";
 
-const ThemeContext = createContext(theme['green']['light']);
+const ThemeContext = createContext(theme["green"]["light"]);
 
-export const ThemeProvider: React.FC<{ themeColor: string, darkMode: string, children: ReactNode }> = ({ themeColor, darkMode, children }) => {
+export const ThemeProvider: React.FC<{
+  themeColor: string;
+  darkMode: string;
+  children: ReactNode;
+}> = ({ themeColor, darkMode, children }) => {
+  // 에러 : typecheck 단계에서 객체를 읽는 key값으로써 신뢰도 문제가 생긴 것으로 보임.
   const selectedTheme = theme[themeColor][darkMode];
   return (
     <ThemeContext.Provider value={selectedTheme}>
--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -48,7 +48,7 @@ export interface ButtonProps {
   /**
    * 버튼이 전체 너비를 차지하는지에 대한 플래그, width 100%
    */
-  fullWidth?: boolean
+  fullWidth?: boolean;
   /**
    * 텍스트 앞쪽으로 자리할 아이콘 요소
    */
@@ -70,9 +70,9 @@ export const Button = ({
   isSelected = false,
   isLoading = false,
   isDisabled = false,
-  variant = 'contained',
-  size = 'medium',
-  color = 'primary',
+  variant = "contained",
+  size = "medium",
+  color = "primary",
   fullWidth = false,
   beforeIcon,
   afterIcon,
@@ -81,13 +81,13 @@ export const Button = ({
    * 현재는 내부 디자인 시스템이 없는 상황이기 때문에 간단한 구현에 초점을 두고
    * loading 혹은 disabled의 경우의 디자인을 별도로 두지 않고 opacity만을
    * CSS로 간단하게 조정하는 방식을 채택하였습니다.
-   * 
+   *
    * 다만, 후에 이를 조정해야 할 필요성이 생긴다면 특수 상태 플래그를 CSS에 넘기는 대신
    * style 조정 hook에 편입시켜 고도화하는 방식을 택할 수 있겠습니다.
-   */  
+   */
 
   // 기본 상태의 스타일 관련 hook 호출
-  const colorStyle = useButtonColorStyle({variant, color, isSelected});
+  const colorStyle = useButtonColorStyle({ variant, color, isSelected });
   const sizeStyle = useButtonSizeStyle({ fullWidth });
 
   // 특수 상태 플래그는 컴포넌트에 넘겨서 css 파일에서 적용
@@ -100,10 +100,11 @@ export const Button = ({
       data-is-loading={isLoading}
       data-is-disabled={isDisabled}
       disabled={isLoading || isDisabled}
-      style={{...colorStyle, ...sizeStyle}}
+      // 에러 : typecheck 단계에서 style에 들어갈 수 있는 형태의 객체인지 확인을 못한 것으로 보임.
+      style={{ ...colorStyle, ...sizeStyle }}
     >
       {beforeIcon && <span className="icon-before">{beforeIcon}</span>}
-      {/* {isLoading ? 로딩 아이콘 : */children}
+      {/* {isLoading ? 로딩 아이콘 : */ children}
       {afterIcon && <span className="icon-after">{afterIcon}</span>}
     </button>
   );
