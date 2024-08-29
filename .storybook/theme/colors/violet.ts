759d67aabc8a432bb3ab034bc9ba2e3e7cb0f151
Hyunbin Jun <gosqlsz@gmail.com>
2024-08-29 15:52:21 +0900

    ✨ [color theme] palette & green theme

---
+++ .storybook/theme/colors/violet.ts
@@ -0,0 +1,41 @@
+import palette from "./palette";
+
+const green = {
+  light: {
+    primary: {
+      weak: palette.green[200],
+      moderate: palette.green[500],
+      strong: palette.green[800],
+    },
+    secondary: {
+      weak: palette.violet[200],
+      moderate: palette.violet[500],
+      strong: palette.violet[700],
+    },
+    success: {
+      weak: palette.success[200],
+      moderate: palette.success[500],
+      strong: palette.success[700],
+    },
+    warning: {
+      weak: palette.warning[200],
+      moderate: palette.warning[500],
+      strong: palette.warning[700],
+    },
+    danger: {
+      weak: palette.danger[200],
+      moderate: palette.danger[500],
+      strong: palette.danger[700],
+    },
+    grayscale: {
+      weak: palette.grayscale[200],
+      moderate: palette.grayscale[500],
+      strong: palette.grayscale[800],
+    },
+    white: "ffffff",
+    black: "000000",
+  },
+  dark: {},
+};
+
+export default green;
