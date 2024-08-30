bf2ba99cba9b389a606931dc5fe2673696aa0b6c
Hyunbin Jun <gosqlsz@gmail.com>
2024-08-30 14:58:53 +0900

    🎨 [theme] type of theme used with typescript

--- .storybook/ThemeContext.tsx
+++ .storybook/ThemeContext.tsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext, ReactNode } from 'react';
-import theme from './theme';
+import theme, { darkModeType, themeColorType, themeType } from './theme';
 
 const ThemeContext = createContext(theme['green']['light']);
 
-export const ThemeProvider: React.FC<{ themeColor: string, darkMode: string, children: ReactNode }> = ({ themeColor, darkMode, children }) => {
-  const selectedTheme = theme[themeColor][darkMode];
+export const ThemeProvider: React.FC<{ themeColor: themeColorType, darkMode: darkModeType, children: ReactNode }> = ({ themeColor, darkMode, children }) => {
+  const selectedTheme : themeType = theme[themeColor][darkMode];
   return (
-    <ThemeContext.Provider value={selectedTheme}>
+    <ThemeContext.Provider value={selectedTheme || theme['green']['light']}>
       {children}
     </ThemeContext.Provider>
   );
