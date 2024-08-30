e4c64ad663a8feac972df47bc804143c980021b9
Hyunbin Jun <gosqlsz@gmail.com>
2024-08-30 14:58:12 +0900

    🎨 [theme] dummy theme imported

--- .storybook/theme/colors/violet.ts
+++ .storybook/theme/colors/violet.ts
@@ -32,10 +32,43 @@ const green = {
       moderate: palette.grayscale[500],
       strong: palette.grayscale[800],
     },
-    white: "ffffff",
-    black: "000000",
+    white: "#ffffff",
+    black: "#000000",
   },
-  dark: {},
+  dark: {
+    primary: {
+      weak: palette.green[200],
+      moderate: palette.green[500],
+      strong: palette.green[800],
+    },
+    secondary: {
+      weak: palette.violet[200],
+      moderate: palette.violet[500],
+      strong: palette.violet[700],
+    },
+    success: {
+      weak: palette.success[200],
+      moderate: palette.success[500],
+      strong: palette.success[700],
+    },
+    warning: {
+      weak: palette.warning[200],
+      moderate: palette.warning[500],
+      strong: palette.warning[700],
+    },
+    danger: {
+      weak: palette.danger[200],
+      moderate: palette.danger[500],
+      strong: palette.danger[700],
+    },
+    grayscale: {
+      weak: palette.grayscale[200],
+      moderate: palette.grayscale[500],
+      strong: palette.grayscale[800],
+    },
+    white: "#ffffff",
+    black: "#000000",
+  },
 };
 
 export default green;
