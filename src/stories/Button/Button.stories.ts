4da81e34c109027bb590b9b10f0e7cdcffd3a595
Hyunbin Jun <gosqlsz@gmail.com>
2024-09-04 12:27:33 +0900

    🎨 [figma] addon installed

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -8,6 +8,10 @@ const meta = {
   component: Button,
   parameters: {
     layout: "centered",
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/MuLoA3USWZiZLS2G7jy5eZ/%EB%94%94%EC%9E%90%EC%9D%B8-%EC%8B%9C%EC%8A%A4%ED%85%9C?node-id=102-1707&t=RbuIwZSI8yrlehCP-1",
+    },
   },
   tags: ["autodocs"],
   // storybook 화면에서 controls로 띄워줄 아이들
