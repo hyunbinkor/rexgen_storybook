9129b0b8dadeb9933297063cdc9b5cb28d66dea4
Hyunbin Jun <gosqlsz@gmail.com>
2024-09-05 15:44:59 +0900

    🚀 [figma] link with figma

--- src/stories/IconButton/IconButton.stories.ts
+++ src/stories/IconButton/IconButton.stories.ts
@@ -2,13 +2,16 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 import { IconButton } from "./IconButton";
 import { iconBtnVariant, iconBtnSize, iconBtnColor } from "./iconButtonType";
-import { Icon } from "../Icon/Icon";
 
 const meta = {
   title: "IconButton",
   component: IconButton,
   parameters: {
     layout: "centered",
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/MuLoA3USWZiZLS2G7jy5eZ/%EB%94%94%EC%9E%90%EC%9D%B8-%EC%8B%9C%EC%8A%A4%ED%85%9C?node-id=251-2060&t=5DO3LkCtumczYipb-1",
+    },
   },
   tags: ["autodocs"],
   // storybook 화면에서 controls로 띄워줄 아이들
