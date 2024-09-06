400e0395bec0ef0ea18bbb9de2dc71173b53cbbf
Hyunbin Jun <gosqlsz@gmail.com>
2024-09-06 14:43:54 +0900

    🎨 [Button] Story figma link updated

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -10,7 +10,7 @@ const meta = {
     layout: "centered",
     design: {
       type: "figma",
-      url: "https://www.figma.com/design/MuLoA3USWZiZLS2G7jy5eZ/%EB%94%94%EC%9E%90%EC%9D%B8-%EC%8B%9C%EC%8A%A4%ED%85%9C?node-id=102-1707&t=RbuIwZSI8yrlehCP-1",
+      url: "https://www.figma.com/design/MuLoA3USWZiZLS2G7jy5eZ/%EB%94%94%EC%9E%90%EC%9D%B8-%EC%8B%9C%EC%8A%A4%ED%85%9C?node-id=246-2847&t=K7SBRBWfM9FpXWoa-1",
     },
   },
   tags: ["autodocs"],
